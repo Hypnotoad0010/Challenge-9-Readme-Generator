d614a6662c2ac6df3fbd2800b45b6fab0b04d5fc
Isaac Harris <IsaacHphotography@gmail.com>
2022-09-08 18:33:31 -0600

    cc

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,93 @@
 // TODO: Include packages needed for this application
 
+//const dependencies created
+const fs = require("fs")
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utlities/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+
+//Questions to be asked for creating your readme
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the Projects Title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please descripe if any, the installation process: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use the application?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please make a list of all contributers"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Any tests that are needed to add?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a licence you would like to use:",
+        choices: ["Apache",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "ODbl"],
+    },
+    {
+        type: "input",
+        name: "GitHub",
+        message: "Please enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email adress: "
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//Function to write readme
+function writeToFile(fileName, data) {
+    if (!fs.existsSync('output')) {
+      fs.mkdirSync('output');
+    }
+    fs.writeFileSync(`./output/${fileName}`, data, (err) =>
+      err ? console.error(err) : console.log("Readme Created")
+    )
+  }
 
 // TODO: Create a function to initialize app
-function init() {}
+//async function added 
+async function init() {
+    return inquirer
+    // Ask user questions
+      .prompt(questions)
+      //Generates responses
+      .then((answers) => {
+        const results = generateMarkdown(answers)
+        writeToFile("README.md", results)
+      })
+      .catch((err) => {
+        console.log(err)
+    })
+
+  }
 
 // Function call to initialize app
-init();
+init()
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,17 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+//Licence badges added
+function renderLicenseBadge =  (license) => {
+  const badges = {
+    Apache: `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+    GNU: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+    ISC: `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
+    MIT: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+    Mozilla: `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
+    ODbl: `[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)`,
+  }
+  return badges[License]
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseSection(license) {
+  if (license === "Apache" || license === "GNU" || license === "ISC" || license === "MIT"|| license === "Mozilla"|| license === "ODbl") {
+    let badge = renderLicenseBadge(license)
+    return ` ${badge} `
+  } else return
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  ;("")
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown = (data) => {
+  const {title, description, installation, usage, contributing, tests, license, GitHub, email} = data
+  let renderedLicense = renderLicenseSection(license)
   return `# ${data.title}
 
 `;
